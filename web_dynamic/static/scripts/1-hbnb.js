2b27e05630ebe1b9ce2e301674d3d004d1e7479a
stak21 <stakashima21@gmail.com>
2019-05-18 19:20:28 +0000

    feat(hbnb scripts): task2 - Javascript that creates a list of all the checked checkboxes and displays them in the amenities h4

---
+++ web_dynamic/static/scripts/1-hbnb.js
@@ -0,0 +1,16 @@
+#!/usr/bin/node
+// Function called when the document is ready
+const ready = () => {
+  // When the checkbox is changed, creates a new list of all the checked boxes
+  // and displays it
+  $('input:checkbox').change(function () {
+      let amenities_list = []
+      let amenities_h4 = $("div.amenities h4")
+      amenities_h4.html("");
+      $.each($("input:checked"), function () {
+        amenities_list.push($(this).attr('data-name'))
+      })
+      amenities_h4.html(amenities_list.join(', '))
+    })
+}
+document.addEventListener("DOMContentLoaded", ready);
