e2d8986509fbc3d9ac188dcf3d7420a70740efd0
stak21 <stakashima21@gmail.com>
2019-05-19 00:05:44 +0000

    feat(hbnb javascript): requests a api

---
+++ web_dynamic/static/scripts/2-hbnb.js
@@ -0,0 +1,37 @@
+#!/usr/bin/node
+// Function called when the document is ready
+const ready = () => {
+  // When the checkbox is changed, creates a new list of all the checked boxes
+  // and displays it
+  $('input:checkbox').change(function () {
+      let amenities_list = []
+      let amenities_h4 = $("div.amenities h4")
+      amenities_h4.html("");
+      $.each($("input:checked"), function () {
+        amenities_list.push($(this).attr('data-name'))
+      })
+      amenities_h4.html(amenities_list.join(', '))
+    })
+
+  // Get the status of the api
+  // let status_url = "https://0.0.0.0:5001/api/v1/status/";
+  // $.get(status_url, {}, (data, textStatus) => {
+    // if (textStatus === 200)
+      // $("div#api_status").addClass("available");
+    // else
+      // $("div#api_status").removeClass("available");
+  // }
+
+  // Send a POST request and send a empty dictionary
+    //loop into the result of the request and create an ARTICLE tag representing
+    //a PLACE in the SECTION.places
+  let places_search_url = "http://0.0.0.0:5001/api/v1/places_search";
+  console.log(places_search_url)
+  $.post(places_search_url, {}, (data, textStatus) => {
+    console.log('hi')
+    console.log(textStatus)
+    console.log(data)
+  })
+
+}
+document.addEventListener("DOMContentLoaded", ready);
